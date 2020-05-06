a136fed5231de77221906df477e9ee591cfde8dc
Alf Nielsen <alfnielsen@gmail.com>
2020-05-06 22:52:06 +0200

    Update useProduceState.ts

--- useProduceState.ts
+++ useProduceState.ts
@@ -1,12 +1,12 @@
 import produce, { Draft } from "immer"
 import { useState } from "react"
 
-const useProduceState = <T>(val: T): [T, (recipe: (draft: Draft<T>) => void) => void] => {
+const useProduceState = <T>(val: T): [T, (recipe: (draft: Draft<T>) => void) => void, React.Dispatch<React.SetStateAction<T>>] => {
    const [v, setV] = useState(val)
    const setter = (recipe: (draft: Draft<T>) => void) => {
       setV(produce(v, recipe))
    }
-   return [v, setter]
+   return [v, setter, setV]
 }
 
 export default useProduceState
